ca11cadecf00367499b525d403d484212a7b6652
r253hmdryou <r253.hmdryou@gmail.com>
2023-10-24 23:01:52 +0900

    lint / prettier導入

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/App.tsx",
+  entry: "./src/Index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "app.js",
